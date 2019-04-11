4446df7a883cebee840f0eb6662e6853e995c823
mmariano <marcusney@gmail.com>
2019-04-11 17:17:26 -0300

    add image, hashbird.kv

--- main.py
+++ main.py
@@ -11,18 +11,34 @@ License: GNU GENERAL PUBLIC LICENSE Version 3
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.image import Image
+
+
+class Manager(ScreenManager):
+    """Class Manager Menu."""
+    pass
+
+
+class Menu(Screen):
+    """Class Menu."""
+    pass
+
+
+class Game(Screen):
+    """Class Game."""
+    pass
+
+
+class Player(Image):
+    """Class Player."""
+    pass
 
 
 class HashBird(App):
     """Main Class Hasbird."""
 
-    def build(self):
-        """Build Method."""
-        player = Image(source='img/player.png')
-        layout = FloatLayout()
-        layout.add_widget(player)
-        return layout        
+    pass
 
 
 HashBird().run()
